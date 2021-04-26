130dd7f9235270cae1fb64b9e77a29209c812351
from97 <skm1104@daum.net>
2021-04-27 01:07:16 +0900

    profile layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import './App.css'
+
+function Header() {
+  return (
+    <header>
+      <h1>Pepple</h1>
+    </header>
+  );
+}
 
 function Profile() {
   return (
-    <div className="Profile">Profile</div>
+    <div className="Profile">
+      <div className="profile_img"></div>
+      <p className="user_name">사용자 닉네임</p>
+      <p className="user_job">프론트엔드 개발자</p>
+      <p className="user_introduction">쭈꾸미처럼 맛있게 성장하고 싶은 쭈꾸미 프론트엔드 개발자 입니다. 쭈꾸미처럼 맛있게 성장하고 싶은 쭈꾸미 프론트엔드 개발자 입니다</p>
+      <button className="modify_profile_bttn">개인정보 수정</button>
+      <ul className="sns_list">
+        <li className="sns_item"></li>
+        <li className="sns_item"></li>
+        <li className="sns_item"></li>
+        <li className="sns_item"></li>
+      </ul>
+    </div>
   );
 }
 
@@ -15,7 +36,7 @@ function Room() {
 function App() {
   return (
     <div className="App">
-      Hello
+      <Header />
       <Profile />
       <Room />
     </div>
