38c3d00cca5c571da32e35ebbf3c8122f954b8ff
from97 <skm1104@daum.net>
2021-05-15 21:44:20 +0900

    modal 위치 고정

--- src/components/AddNewRoomModal.js
+++ src/components/AddNewRoomModal.js
@@ -1,9 +1,9 @@
 import React from "react";
-import "./Modal.css";
+import "../Modal.css";
 
 function AddNewRoomModal({ state, closeModal }) {
     return (
-        <div className={ state ? 'openModal' : 'modal'}>
+        <div className={ state ? 'openModal modal' : 'modal'}>
             모달
             <button onClick={closeModal}>닫기</button>
         </div>
